a6820855fd4c6f253cc4578925e813330b9c7b59
ShimanskayaElena <shimanskaya88@tut.by>
2017-10-01 18:08:11 +0300

    to create a demo version of the project

--- src/app/list/list-worker/list-worker.component.ts
+++ src/app/list/list-worker/list-worker.component.ts
@@ -4,6 +4,8 @@ import { ListService } from '../../core/list.service';
 import { Worker } from '../../shared/worker.model';
 import { WorkerComponent } from '../worker/worker.component';
 
+import { data } from '../../shared/data';
+
 @Component({
   selector: 'app-list-worker',
   templateUrl: './list-worker.component.html',
@@ -18,11 +20,20 @@ export class ListWorkerComponent implements OnInit {
   constructor( private listService: ListService) { }
 
   ngOnInit() {
-    this.listService.getListWorker()
+
+    // Так как у репозитория на github (https://github.com/ShimanskayaElena/list-worker)
+    // и у источика данных для приложения (http://jsonplaceholder.typicode.com/users)
+    // разные протоколы у меня не получилось сделать демо версию приложения на gh-pages
+    // поэтому я вынуждена упростить получение данных, чтобы можно было увидеть реализацию этого проекта
+    // это никак не отразилось на его функциональности
+
+    /* this.listService.getListWorker()
       .subscribe(
         data => this.listWorker = data,
         error => this.errorMessage = <any>error
-      );
+      );*/
+
+    this.listWorker = data;
 
     this.selectedWorker = this.listService.getSelectedWorker();
   }
